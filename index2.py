96558cd031712c6bda1b669f5a6a870bab9794bb
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2020-05-01 21:48:42 -0300

    Creating json file with the response of http request

--- index2.py
+++ index2.py
@@ -1,5 +1,13 @@
 import requests
 import json
+
 response = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=11721f372b13c7a37a739108ba8ff3d8085bf205')
 
-print (response.content)
+try:
+    arquivo_json = open("answer.json","wb")
+    arquivo_json.write(response.content)
+    arquivo_json.close()
+
+except Exception as erro:
+    print('Erro:'+format(erro))
+    print('Erro Tipo:'+format(type(erro)))
